22fb137d26cb5294f736636e900d309d6c4b9e46
nazwastpn <nazwasetiapin@gmail.com>
2026-01-20 10:24:06 +0700

    feat: protected route for authenticated pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
-  const token = localStorage.getItem("token");
-
   return (
     <Routes>
-      {/* default redirect */}
-      <Route
-        path="/"
-        element={<Navigate to={token ? "/dashboard" : "/login"} />}
-      />
+      <Route path="/" element={<Navigate to="/dashboard" />} />
 
-      {/* login */}
-      <Route
-        path="/login"
-        element={token ? <Navigate to="/dashboard" /> : <Login />}
-      />
+      <Route path="/login" element={<Login />} />
 
-      {/* dashboard */}
       <Route
         path="/dashboard"
-        element={token ? <Dashboard /> : <Navigate to="/login" />}
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
       />
     </Routes>
   );
